5e6155ba583d3ccbf57d35f8aa93c565f8d3d2eb
dwbrown115 <114196159+dwbrown115@users.noreply.github.com>
2023-07-30 02:20:11 -0500

    weird bug appeared that prevented localHost from connecting

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-        port: 3000
-          }
-})
+    plugins: [react()],
+    server: {
+        port: 3000,
+        // https: {
+        //     // https => https://localhost:3000 | http => http://localhost:3000
+        //     maxSessionMemory: 100,
+        // },
+    },
+});
