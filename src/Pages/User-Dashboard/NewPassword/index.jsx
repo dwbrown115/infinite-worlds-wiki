c2148dc409052d8f99d3048e8a6e86ab82264404
dwbrown115 <114196159+dwbrown115@users.noreply.github.com>
2023-08-15 23:57:54 -0500

    fixed more imports

--- src/Pages/User-Dashboard/NewPassword/index.jsx
+++ src/Pages/User-Dashboard/NewPassword/index.jsx
@@ -7,9 +7,9 @@ import {
 } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
 
-import { firebase_app, logOut } from "../../../firebase";
+import { firebase_app } from "../../../firebase";
 // import firebase_app from "../../../firebase/config";
-// import logOut from "../../../firebase/auth/signout";
+import logOut from "../../../firebase/auth/signout";
 
 function NewPassword() {
     const auth = getAuth(firebase_app);
